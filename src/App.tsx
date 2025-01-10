0aa71de4d103a9f555bb75c35059b4ef71948345
Hiroyuki Tanjo <expheno@gmail.com>
2025-01-10 23:13:54 +0900

    Install google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import { useCallback, useEffect, useState } from 'react';
+import ReactGA from 'react-ga4';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 import classes from './App.module.scss';
-import { useCallback, useState } from 'react';
 
 function App() {
   const [activeMenuId, setActiveMenuId] = useState('');
   const handleChangeSection = useCallback((id: string) => {
     setActiveMenuId(id);
   }, []);
+
+  useEffect(() => {
+    if (import.meta.env.PROD) {
+      ReactGA.initialize('G-MPT0YVV9GQ', {
+        gtagOptions: { send_page_view: false },
+      });
+      ReactGA.send({
+        hitType: 'pageview',
+        title: document.title,
+      });
+    }
+  }, []);
+
   return (
     <div className={classes.app}>
       <Header activeMenuId={activeMenuId} />
